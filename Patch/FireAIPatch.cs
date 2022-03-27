398dddb59f09d6b95da6736743079da3fd569fcb
TheDogKSP <cambrium@abwesend.de>
2022-03-27 20:32:04 +0200

    fireaipatch retargetcims adapt distance

--- Patch/FireAIPatch.cs
+++ Patch/FireAIPatch.cs
@@ -105,6 +105,10 @@ namespace MoreEffectiveTransfer.Patch.Fire
 
         public static void TargetCimsParentVehicleTarget(ushort vehicleID, ref Vehicle vehicleData)
         {
+            //not close enough? return
+            if (Vector3.SqrMagnitude(vehicleData.GetLastFramePosition() - Singleton<BuildingManager>.instance.m_buildings.m_buffer[vehicleData.m_targetBuilding].m_position) > 50 * 50)
+                return;
+
             CitizenManager instance = Singleton<CitizenManager>.instance;
             uint numCitizenUnits = instance.m_units.m_size;
             uint num = vehicleData.m_citizenUnits;
@@ -193,9 +197,9 @@ namespace MoreEffectiveTransfer.Patch.Fire
         [HarmonyPostfix]
         public static void Postfix(ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics)
         {
+            /*
             if ((vehicleData.m_flags & (Vehicle.Flags.GoingBack | Vehicle.Flags.WaitingTarget)) != 0)
             {
-                /*
                 ushort newTarget = FireAIPatch.FindBuildingWithFire(vehicleData.GetLastFramePosition(), FireAIPatch.FIRE_DISTANCE_SEARCH);
                 if (newTarget != 0)
                 {
@@ -219,7 +223,7 @@ namespace MoreEffectiveTransfer.Patch.Fire
 #endif
 
                 }
-                */
+
             }
             else if ((vehicleData.m_targetBuilding != 0) && (Singleton<BuildingManager>.instance.m_buildings.m_buffer[vehicleData.m_targetBuilding].m_fireIntensity == 0))
             {
@@ -227,6 +231,7 @@ namespace MoreEffectiveTransfer.Patch.Fire
                 vehicleData.Info.m_vehicleAI.SetTarget(vehicleID, ref vehicleData, 0); //clear target
                 FireAIPatch.dynamic_redispatch_counter++;
             }
+            */
         }
 
     }
